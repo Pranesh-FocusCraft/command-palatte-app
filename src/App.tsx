ad827af8c5b6b79a5581a0fb7d8d1abb09ec62a4
Pranesh <pranesh.b.k@focuscrafttech.com>
2024-01-18 00:57:52 +0530

    !!! redo commit temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { ChangeEvent } from 'react'
 import './App.css'
+import { useSignal } from '@preact/signals-react'
 
 function App() {
-	return <></>
+	const data = useSignal('')
+
+	const handleChange = ({
+		target: { value },
+	}: ChangeEvent<HTMLInputElement>) => {
+		data.value = value
+	}
+
+	return (
+		<div className='MainPage'>
+			<input onChange={handleChange} />
+		</div>
+	)
 }
 
 export default App
